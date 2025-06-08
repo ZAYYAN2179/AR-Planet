f429949c41f9491d65cf0eb7b3a4fdd66bd058e0
Muh Zayyan Al Thaaf Nur <aatzayyan49@gmail.com>
2025-06-09 00:34:02 +0700

    Info Planet

---
+++ Assets/Script/BackLandingPage.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackLandingPage : MonoBehaviour
+{
+    public void LoadSampleScene()
+    {
+        SceneManager.LoadScene("LandingPage");
+    }
+}
